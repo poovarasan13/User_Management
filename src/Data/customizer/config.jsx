1b8be106e7be7d5bd2d1ef29c729dfb7ad2bf091
Simson M <sims91502@gmail.com>
2025-02-09 09:58:17 +0530

    feat: Theme config added

---
+++ src/Data/customizer/config.jsx
@@ -0,0 +1,18 @@
+export class ConfigDB {
+    static data = {
+        settings: {
+            layout_type: 'ltr',
+            sidebar: {
+                type: 'compact-wrapper modern-type',
+                iconType: 'stroke-svg',
+            },
+            sidebar_setting: '',
+        },
+        color: {
+            primary_color: '#7366ff',
+            secondary_color: '#f73164',
+            mix_background_layout: 'light-only',
+        },
+        router_animation: 'fadeIn'
+    }
+}
